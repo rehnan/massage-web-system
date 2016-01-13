37876e1ecf89aee59ef61c160325f57b7da59179
Rehnan Carolino <rehnancarolino@live.com>
2016-01-12 23:12:25 -0200

    teste

--- tulio_site/wp-content/themes/tulio/header.php
+++ tulio_site/wp-content/themes/tulio/header.php
@@ -20,31 +20,131 @@
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tulio' ); ?></a>
+<!-- TOPO -->
+	<header class="topo">
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<div id="owl-demo">
+      		<?php 
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+				$destaquesPost = new WP_Query( array( 'post_type' => 'destaques', 'orderby' => 'id', 'order' => 'asc', 'posts_per_page' => -1 ) );
+                
+                while ( $destaquesPost->have_posts() ) : $destaquesPost->the_post();
+				
+				$foto = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+				$foto = $foto[0];
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tulio' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			?>
+				<div class="item" style="background: url(<?php echo "$foto";?> ">
+					<div class="row">
+					<div class="col-md-12">
+						<div class="frase">
 
+								<span><i><?php echo the_title(); ?> </i></span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php endwhile; wp_reset_query(); ?>	
+				
+
+				
+				
+				
+		</div>
+
+		
+	
+
+		<header class="area-topo">
+
+			<div class="navbar" role="navigation">
+
+				<div class="container">
+
+					<!-- MENU MOBILE TRIGGER -->
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse">
+						<span class="sr-only"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+
+					<!-- LOGOTIPO / MENU MOBILE-->
+					<div class="row navbar-header">
+
+
+						<!-- LOGO -->
+						<div class="col-md-4">
+							<a href="#" title="Stetica">
+								<h1 class="img-responsive">
+									Túlio Tobias Massoterapeuta
+								</h1>
+							</a>
+						
+						</div>
+
+						<!-- MENU -->
+						<div class="col-md-8">
+
+							<nav class="collapse navbar-collapse" id="collapse">
+
+								<ul class="nav navbar-nav">
+
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Massagem estética<span class="fa fa-angle-down"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Drenagem</a></li>
+											<li><a href="#">Relaxante </a></li>
+											<li><a href="#">Desportiva</a></li>
+											
+											
+										</ul>
+									</li>
+
+								
+
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Terapêutica  <span class="fa fa-angle-down"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Reflexologia </a></li>
+											<li><a href="#">Drenagem Linfática</a></li>
+											
+											
+											<li><a href="#">Quick Massage</a></li>
+											
+										</ul>
+									</li>
+
+
+									<li><a href="#">Blog</a></li>
+
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cursos <span class="fa fa-angle-down"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Estética facial</a></li>
+											<li><a href="#">Micropig Estética</a></li>
+											<li><a href="#">Micropig. paramédica</a></li>
+										</ul>
+									</li>
+									
+									<li><a href="#">Quem Somos</a></li>
+									<li><a href="#">Contato</a></li>
+
+								</ul>
+
+							</nav>
+							
+						</div>
+
+					</div>
+
+				</div>
+
+				
+
+			</div>
+		
+		</header>
+	
+	</header>
 	<div id="content" class="site-content">
